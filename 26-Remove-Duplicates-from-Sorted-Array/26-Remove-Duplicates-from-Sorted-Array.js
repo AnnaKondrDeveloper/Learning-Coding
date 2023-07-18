16c9883a96007fbb302f7cd16531c738fb84d45d
Anna Kondrateva <85047120+AnnaKondrDeveloper@users.noreply.github.com>
2023-07-17 23:22:18 -0400

    26-Remove-Duplicates-from-Sorted-Array

---
+++ 26-Remove-Duplicates-from-Sorted-Array/26-Remove-Duplicates-from-Sorted-Array.js
@@ -0,0 +1,12 @@
+var removeDuplicates = function(nums) {
+	if(nums.length <= 1) {
+		 return nums.length;
+	}
+	var k = 1;
+	for(var i = 1; i < nums.length; i++) {
+		 if(nums[i] != nums[i-1]){
+			  nums[k++] = nums[i];
+		 }
+	}
+	return k;
+};
